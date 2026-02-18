f164f3f0f842c22a4c6008c170dbde1dc28233ee
Sanket Puri Goswami <sanketpurigoswami@gmail.com>
2026-02-18 09:46:42 +0530

    Add files via upload

---
+++ calculator_dti.py
@@ -0,0 +1,40 @@
+import re
+
+result = input("enter expression: ")
+aftersplit = re.split(r"([-+*/])", result)
+print(aftersplit)
+
+while len(aftersplit) >1:
+    for i in (aftersplit):
+        if i in ['/', '*']:
+            x = aftersplit.index(i)
+            a = aftersplit[x-1]
+            b = aftersplit[x+1]
+            result = float(a)/float(b) 
+            aftersplit[x] = result
+            aftersplit.pop(x+1)
+            aftersplit.pop(x-1)
+    print(aftersplit)
+    for i in (aftersplit):
+        if i in ['*']:
+            x = aftersplit.index(i)
+            a = aftersplit[x-1]
+            b = aftersplit[x+1]
+            result = float(a)*float(b)
+            aftersplit[x] = result
+            aftersplit.pop(x+1)
+            aftersplit.pop(x-1)
+            
+    print(aftersplit)
+    for i in (aftersplit):
+        if i in ['+','-']:
+            x = aftersplit.index(i)
+            a = aftersplit[x-1]
+            b = aftersplit[x+1]
+            result = float(a)+float(b) if i == '+' else float(a)-float(b)
+            aftersplit[x] = result
+            aftersplit.pop(x+1)
+            aftersplit.pop(x-1)
+    print(aftersplit)
+print(aftersplit)
+        
